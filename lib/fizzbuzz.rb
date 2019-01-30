a1292b9f711469036bd3a6034fe79129a9430121
tso402 <james.ault@johnlewis.co.uk>
2019-01-30 16:19:08 +0000

    Passing test that this is not an integer is returned when passed a boolean

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(input)
-   if input % 3 == 0 && input % 5 == 0
+  if !input.is_a? Integer
+    return "this is not an integer"
+  elsif input % 3 == 0 && input % 5 == 0
     return 'fizzbuzz'
   elsif input % 3 == 0
     return 'fizz'
   elsif input % 5 == 0
     return 'buzz'
-  elsif !input.is_a? Integer
-    return "this is not an integer"
   else
     return input
   end  
