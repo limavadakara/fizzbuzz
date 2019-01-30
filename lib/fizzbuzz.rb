8af7b2e7bacd462c2c6e8b4e9a1e703ec98157b1
Lima Vadakara <lima.vadakara@gmail.com>
2019-01-30 15:55:07 +0000

    Pass test that input is returned when passed 13

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,13 @@
 def fizzbuzz(input)
    if input % 3 == 0 && input % 5 == 0
     return 'fizzbuzz'
-    elsif input == 1
-    return input
   elsif input % 3 == 0
     return 'fizz'
   elsif input % 5 == 0
     return 'buzz'
   elsif !input.is_a? Integer
     return "this is not an integer"
-   end  
+  else
+    return input
+  end  
 end
